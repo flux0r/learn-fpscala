da3d5a43b706f57fd38d5d1d11006c5eeec643cf
flux0r <flux0r@brettmingalls.com>
2013-06-19 23:30:47 -0700

    Clean up and add gitignore.

--- blank.R
+++
@@ -1,9 +0,0 @@
-xs <- bd.coerce(cox[, c(
-        grep("online.*exp", names(cox), value=TRUE),
-        grep("vlist", names(cox), value=TRUE),
-        "campaign")])
-xs <- xs[xs$campaign == "rp13.q1"]
-
-t <- table(xs$vlist, as.numeric(xs$rp13.q1.online.total.overall.exp > 0))
-total.respondents <- sum(t)
-total.respondents.online.exp <- sum(t[, 2])
